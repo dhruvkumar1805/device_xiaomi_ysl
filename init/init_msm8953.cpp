e41ee3935e0a979b424205c8dbb9a73b9242de78
Demon000 <demonsingur@gmail.com>
2021-06-11 16:13:23 +0000

    ysl: init: set device name according to region
    Signed-off-by: Sandeep Chahal <sandeeps567ingh@gmail.com>

--- init/init_msm8953.cpp
+++ init/init_msm8953.cpp
@@ -1,7 +1,5 @@
 /*
    Copyright (c) 2016, The CyanogenMod Project
-   Copyright (c) 2019, The LineageOS Project
-
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
    met:
@@ -14,7 +12,6 @@
     * Neither the name of The Linux Foundation nor the names of its
       contributors may be used to endorse or promote products derived
       from this software without specific prior written permission.
-
    THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
    WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
@@ -29,30 +26,34 @@
  */
 
 #include <cstdlib>
-#include <fstream>
-#include <string.h>
+#include <cstring>
 #include <sys/sysinfo.h>
-#include <unistd.h>
-
+#include <vector>
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
 #include "vendor_init.h"
 #include "property_service.h"
+#include "log/log.h"
+#include <string.h>
+
+char const *heapgrowthlimit;
+char const *heapminfree;
 
 using android::base::GetProperty;
 
-char const *heapstartsize;
-char const *heapgrowthlimit;
-char const *heapsize;
-char const *heapminfree;
-char const *heapmaxfree;
-char const *heaptargetutilization;
+std::vector<std::string> ro_props_default_source_order = {
+    "",
+    "odm.",
+    "product.",
+    "system.",
+    "vendor.",
+    "system_ext.",
+};
 
-void property_override(char const prop[], char const value[], bool add = true)
-{
-    auto pi = (prop_info *) __system_property_find(prop);
+void property_override(char const prop[], char const value[], bool add = true) {
+    auto pi = (prop_info *)__system_property_find(prop);
 
     if (pi != nullptr) {
         __system_property_update(pi, value, strlen(value));
@@ -61,6 +62,40 @@ void property_override(char const prop[], char const value[], bool add = true)
     }
 }
 
+void set_ro_build_prop(const std::string &source, const std::string &prop,
+                       const std::string &value, bool product = false) {
+    std::string prop_name;
+
+    if (product) {
+        prop_name = "ro.product." + source + prop;
+    } else {
+        prop_name = "ro." + source + "build." + prop;
+    }
+
+    property_override(prop_name.c_str(), value.c_str(), false);
+}
+
+void set_device_props(const std::string brand, const std::string device, const std::string model) {
+    for (const auto &source : ro_props_default_source_order) {
+        set_ro_build_prop(source, "brand", brand, true);
+        set_ro_build_prop(source, "device", device, true);
+        set_ro_build_prop(source, "model", model, true);
+    }
+
+}
+
+void load_device_properties() {
+    std::string region = GetProperty("ro.boot.hwcountry", "");
+
+    if (region == "Global") {
+            set_device_props("Xiaomi", "ysl", "Redmi S2");
+        } else if (region == "India") {
+            set_device_props("Xiaomi", "ysl", "Redmi Y2");
+	    
+   }
+}
+
+	
 void check_device()
 {
     struct sysinfo sys;
@@ -68,51 +103,35 @@ void check_device()
     sysinfo(&sys);
 
     if (sys.totalram > 3072ull * 1024 * 1024) {
-        // from - phone-xxhdpi-4096-dalvik-heap.mk
-        heapstartsize = "8m";
+       // from - Stock rom
         heapgrowthlimit = "256m";
-        heapsize = "512m";
-        heaptargetutilization = "0.6";
-        heapminfree = "8m";
-        heapmaxfree = "16m";
+        heapminfree = "4m";
     } else {
-        // from - phone-xhdpi-2048-dalvik-heap.mk
-        heapstartsize = "8m";
+        // from - phone-xxhdpi-2048-dalvik-heap.mk
         heapgrowthlimit = "192m";
-        heapsize = "512m";
-        heaptargetutilization = "0.75";
-        heapminfree = "512k";
-        heapmaxfree = "8m";
+        heapminfree = "2m";
+   }
+}
+
+void set_avoid_gfxaccel_config() {
+    struct sysinfo sys;
+    sysinfo(&sys);
+
+    if (sys.totalram <= 3072ull * 1024 * 1024) {
+        // Reduce memory footprint
+        property_override("ro.config.avoid_gfx_accel", "true");
     }
 }
-void load_ysl() {
-    property_override("ro.product.model", "Redmi S2");
-    property_override("ro.build.product", "ysl");
-    property_override("ro.product.device", "ysl");
-}
 
-void load_ysl_india() {
-    property_override("ro.product.model", "Redmi Y2");
-    property_override("ro.build.product", "ysl");
-    property_override("ro.product.device", "ysl");
-}
-
-void vendor_load_properties()
-{ 
-     std::string region = android::base::GetProperty("ro.boot.hwcountry", "");
-
-	if (region.find("INDIA") != std::string::npos) {
-        load_ysl_india();
-	}	
-	else{
-        load_ysl();		
-	}
+void vendor_load_properties() {
     check_device();
+	set_avoid_gfxaccel_config();
+	load_device_properties();
 
-    property_override("dalvik.vm.heapstartsize", heapstartsize);
+    property_override("dalvik.vm.heapstartsize", "16m");
     property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
-    property_override("dalvik.vm.heapsize", heapsize);
-    property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
+    property_override("dalvik.vm.heapsize", "512m");
+    property_override("dalvik.vm.heaptargetutilization", "0.75");
     property_override("dalvik.vm.heapminfree", heapminfree);
-    property_override("dalvik.vm.heapmaxfree", heapmaxfree);
+    property_override("dalvik.vm.heapmaxfree", "8m");
 }
